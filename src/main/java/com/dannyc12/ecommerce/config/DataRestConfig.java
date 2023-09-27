69a340121e03d268d3d9b28683faff239e3b88dc
dannyc12 <danny.clynes12@gmail.com>
2023-09-27 14:47:56 -0500

    update DataRestConfig

--- src/main/java/com/dannyc12/ecommerce/config/DataRestConfig.java
+++ src/main/java/com/dannyc12/ecommerce/config/DataRestConfig.java
@@ -50,7 +50,6 @@ public class DataRestConfig implements RepositoryRestConfigurer {
     }
 
     private void exposeIds(RepositoryRestConfiguration config) {
-        // expose entity ids
 
         // get a list of all entity classes from the entity manager
         Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
